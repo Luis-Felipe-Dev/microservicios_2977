29f64ab60518a83205b07d4dd1a31a4a465e9bba
Luis-Felipe-Dev <luis.fhb.2016@gmail.com>
2023-05-17 08:31:27 -0500

    fisrt commit

---
+++ src/main/java/pe/isil/microservicios_2977/Microservicios2977Application.java
@@ -0,0 +1,13 @@
+package pe.isil.microservicios_2977;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservicios2977Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservicios2977Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/isil/microservicios_2977/Microservicios2977ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.isil.microservicios_2977;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservicios2977ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
